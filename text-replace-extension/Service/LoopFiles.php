53e56f9166a6f52ec68b09fb34706a9ab5837cb7
Tomislav <tomislav.kovacic11@gmail.com>
2016-08-16 01:11:28 +0200

    Update LoopFiles.php

--- text-replace-extension/Service/LoopFiles.php
+++ text-replace-extension/Service/LoopFiles.php
@@ -1,37 +1,40 @@
 <?php
 
 $dir = 'simboli';
-$path = 'http://localhost/Service/simboli'; //url simbola na poslužitelju
+
+//url simbola na poslužitelju
+$path = 'http://localhost/Service/simboli';
 
 $array = array();
 
 $array = LoopFiles($dir,$path,$array);
 
-function LoopFiles($dir,$path,$array)
-{
-	$files = scandir($dir); // čitanje svih datoteka u trenutnom direktoriju
-	foreach ($files as $file)
-	{
-		if ($file!='.' && $file!='..' )
-		{
-			if (is_dir($dir.'\\'.$file)) // ako je direktorij onda ponovno pozovi funkciju sa novim parametrom
-	        {	
+function LoopFiles($dir,$path,$array) {
+	// čitanje svih datoteka u trenutnom direktoriju
+	$files = scandir($dir);
+	foreach ($files as $file) {
+		if ($file!='.' && $file!='..' ) {
+			// ako je direktorij onda ponovno pozovi funkciju sa novim parametrom
+			if (is_dir($dir.'\\'.$file)) {	
 				$array = LoopFiles($dir.'\\'.$file, $path.'/'.$file, $array);
 			}
-			else
-			{
-				$filename = pathinfo($dir.'\\'.$file)['filename']; // čitanje imena datoteka
-				$filename = utf8_encode($filename); // kodiranje imena utf8
-				$array[' '.$filename.' '] = utf8_encode($path.'/'.$file); // spremanje imena datoteke(simbola) i pripadajućeg url-a u asocijativno polje
+			else {
+				// čitanje imena datoteka
+				$filename = pathinfo($dir.'\\'.$file)['filename'];
+				// kodiranje imena utf8
+				$filename = utf8_encode($filename);
+				// spremanje imena datoteke(simbola) i pripadajućeg url-a u asocijativno polje
+				$array[' '.$filename.' '] = utf8_encode($path.'/'.$file); 
 			}
 		}
 	}
 	return $array;
 }
 
-$json = json_encode($array); //pretvaranje u json format
+//pretvaranje u json format
+$json = json_encode($array);
 
-print ($json); //printanje rezultata
+print ($json);
 
 ?>
 
